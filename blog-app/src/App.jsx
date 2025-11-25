91e3fcb4a3a4c508be52d2fae4390484dfe17bc8
iknoor12 <imiknoor@gmail.com>
2025-11-25 10:07:25 +0530

    Create components

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,8 +1,14 @@
+import PostList from './components/PostList';
+import PostDetails from './components/PostCard';
+import AddPost from './components/AddPost';
 import './App.css'
 
 function App() {
  return (
     <>
+    <PostList />
+    <PostDetails />
+    <AddPost />
     </>
   )
 }
